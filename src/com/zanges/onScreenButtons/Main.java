4d33554004c0fcce5a47338ea7f62b88dd11b939
Zanges Code <zanges.code@gmail.com>
2016-03-28 00:39:06 +0200

    Ini

---
+++ src/com/zanges/onScreenButtons/Main.java
@@ -0,0 +1,8 @@
+package com.zanges.onScreenButtons;
+
+/**
+ * Created by Zanges on 28.03.2016.
+ */
+public class Main {
+
+}
---
+++ src/com/zanges/onScreenButtons/util/helper/LogHelp.java
@@ -0,0 +1,10 @@
+package com.zanges.onScreenButtons.util.helper;
+
+/**
+ * Created by Zanges on 28.03.2016.
+ */
+public class LogHelp {
+    public static void debug (String text){
+        System.out.println(text);
+    }
+}
